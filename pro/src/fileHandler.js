c0abc829daee0458f483074013a8143ca67dbe64
LiFu-ChrisLee <753641657@qq.com>
2020-03-13 14:24:22 +0800

    build for prerelease

---
+++ pro/src/fileHandler.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const colors_1 = __importDefault(require("colors"));
+const utils_1 = require("./utils");
+const config_1 = require("./config");
+function fileHandler() {
+    const pathFile = config_1.DEFAULT_RECORD_FILE;
+    if (!fs_1.default.existsSync(pathFile)) {
+        utils_1.soloConsole.warn('Can not find records, use default config.');
+        const pkgPath = path_1.default.join(utils_1.getUserDir(), config_1.DEFAULT_PKG_FILE_NAME);
+        utils_1.wFile(pathFile, pkgPath, 'Finding ...').then(() => {
+            utils_1.soloConsole.success(`Backup file: ${colors_1.default.blue(pkgPath)}`);
+        });
+    }
+    else {
+        utils_1.rFile(pathFile, 'Finding ...').then(pkgPath => {
+            utils_1.soloConsole.success(`Backup file: ${colors_1.default.blue(pkgPath)}`);
+        });
+    }
+}
+exports.fileHandler = fileHandler;
--- pro/src/installHandler.js
+++ pro/src/installHandler.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const colors_1 = __importDefault(require("colors"));
 const utils_1 = require("./utils");
+const config_1 = require("./config");
 const InstallPackages_1 = require("./installCls/InstallPackages");
 function installHandler(args) {
-    const backupFile = path_1.default.join(utils_1.getUserDir(), 'npm.global.txt');
+    const backupFile = path_1.default.join(utils_1.getUserDir(), config_1.DEFAULT_PKG_FILE_NAME);
     utils_1.rFile(backupFile)
         .then(text => {
         const isp = new InstallPackages_1.InstallPackages(text, args.needVersion);
@@ -16,6 +17,9 @@ function installHandler(args) {
     })
         .then(data => {
         utils_1.soloConsole.success(colors_1.default.white(data));
+    })
+        .then(() => {
+        return utils_1.wFile(config_1.DEFAULT_RECORD_FILE, backupFile, 'Recording ...');
     })
         .catch(e => {
         utils_1.soloConsole.error(e);
