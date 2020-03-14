8b4507d7a19c047bec60881ea0ef19e30a52a144
LiFu-ChrisLee <753641657@qq.com>
2020-03-15 00:02:35 +0800

    build for release

--- pro/index.js
+++ pro/index.js
@@ -4,30 +4,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 const commander_1 = __importDefault(require("commander"));
 const colors_1 = __importDefault(require("colors"));
 const utils_1 = require("./src/utils");
 const backupHandler_1 = require("./src/backupHandler");
 const installHandler_1 = require("./src/installHandler");
 const fileHandler_1 = require("./src/fileHandler");
+const config_1 = require("./src/config");
 const package_json_1 = __importDefault(require("./package.json"));
 commander_1.default.version(package_json_1.default.version).name('backup-global|bkg');
 commander_1.default
     .command('backup')
     .alias('b')
-    .option('-n --no-version', 'backup package with version')
+    .option('-n, --no-version', 'backup package with version')
+    .option('-o, --output <filePath>', 'backup to custom file')
     .description('backup your global packages')
     .action(agrvs => {
-    const options = { needVersion: agrvs.version };
+    let backupFile = config_1.DEFAULT_RECORD_FILE;
+    if (agrvs.output) {
+        backupFile = path_1.default.resolve(process.cwd(), agrvs.output);
+    }
+    const options = {
+        needVersion: agrvs.version,
+        backupFile,
+    };
     backupHandler_1.backupHandler(options);
 });
 commander_1.default
     .command('install')
     .alias('i')
     .description('install your backup')
-    .option('-n --no-version', 'install package with version')
+    .option('-n, --no-version', 'install package with version')
+    .option('-i, --input <filePath>', 'use custom backup file')
     .action(agrvs => {
-    const options = { needVersion: agrvs.version };
+    let backupFile = config_1.DEFAULT_RECORD_FILE;
+    if (agrvs.input) {
+        backupFile = path_1.default.resolve(process.cwd(), agrvs.input);
+    }
+    const options = {
+        needVersion: agrvs.version,
+        backupFile,
+    };
     installHandler_1.installHandler(options);
 });
 commander_1.default
--- pro/src/config.js
+++ pro/src/config.js
@@ -4,7 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
-const DEFAULT_PKG_FILE_NAME = 'npm.global.txt';
-exports.DEFAULT_PKG_FILE_NAME = DEFAULT_PKG_FILE_NAME;
+const utils_1 = require("./utils");
+const DEFAULT_PKG_FILE = path_1.default.join(utils_1.getUserDir(), 'npm.global.txt');
+exports.DEFAULT_PKG_FILE = DEFAULT_PKG_FILE;
 const DEFAULT_RECORD_FILE = path_1.default.join(__dirname, '.pathrc');
 exports.DEFAULT_RECORD_FILE = DEFAULT_RECORD_FILE;
